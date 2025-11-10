425b981e5ae6ef69c12906cf608d77af32d502e7
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2025-11-10 16:33:19 +0530

    Namaste.py

---
+++ 01_Basics/Namaste.py
@@ -0,0 +1,13 @@
+from hello import Namaste
+
+Namaste("Hello Python Community ✌️")
+
+# In this file, we are importing the `Namaste` function from the `hello.py` file and calling it with the argument "Hello Python Community ✌️".
+# This demonstrates how to use functions defined in other files by importing them.
+
+# -------------------------------------------------------------------------------
+
+# Python Internal Working 👇🏻
+
+# Hashnode Article: https://faizaankhan.hashnode.dev/how-python-works-internally
+# GeeksforGeeks Article: https://www.geeksforgeeks.org/python/internal-working-of-python/
