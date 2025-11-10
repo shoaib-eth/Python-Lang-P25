eef25a99129de9a1eb78339e3b20e1fdbfa3d9c0
Shoaib Khan <shoaibkhan@shoaibs-MacBook-Air.local>
2025-11-10 15:43:36 +0530

    def in python

--- 01_Basics/hello.py
+++ 01_Basics/hello.py
@@ -1,1 +1,10 @@
 print("Hello World!")
+
+def Namaste(n):
+    print(n)
+
+Namaste("Hello India!")
+
+# `def` is used to define a function in Python. Here, we define a function named `Namaste` that takes one parameter `n` and prints it. We then call the function with the argument "Hello India!" to demonstrate its functionality.
+
+# we can also use single quotes for strings
